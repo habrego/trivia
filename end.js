eb507f6e6c41c256441a8b432254d98cc872d395
habrego <54692177+habrego@users.noreply.github.com>
2020-10-09 23:30:29 -0500

    Update end.js

--- end.js
+++ end.js
@@ -1,4 +1,4 @@
-//  id=201009
+//  id=201009x
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
